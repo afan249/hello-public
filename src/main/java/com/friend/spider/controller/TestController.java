01cb79ccd66d30a0659e70f6018f735493e7cc6b
fyc <fanyongcheng@vipkid.com>
2018-09-17 11:48:37 +0800

    首次给大家提交代码

---
+++ src/main/java/com/friend/spider/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.friend.spider.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * fyc
+ */
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("")
+    public String home(){
+        return "探活";
+    }
+
+}
+
