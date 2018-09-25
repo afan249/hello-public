6c425b9198071350a27dc00ef5a7b650ea21be74
SuperqiQAQ <4716842032@qq.com>
2018-09-26 00:36:42 +0800

    QAQ提交代码

---
+++ src/main/java/com/friend/spider/test/zhangqi/gitBaseInfo.java
@@ -0,0 +1,8 @@
+package com.friend.spider.test.zhangqi;
+
+public class gitBaseInfo {
+    public static void main(String[] args) {
+        System.out.print("fist project user idea 练习 git 使用");
+
+    }
+}
